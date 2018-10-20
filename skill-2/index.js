69e944b9e06e2240a2e250ab693221d3a47fa212
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-10-20 23:40:08 +0530

    Added skill 2

---
+++ skill-2/index.js
@@ -0,0 +1,249 @@
+const Alexa = require('ask-sdk-core');
+const i18n = require('i18next');
+const sprintf = require('i18next-sprintf-postprocessor');
+const https = require('https');
+
+const LaunchHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        const requestAttributes = attributesManager.getRequestAttributes();
+        const speechOutput = `${requestAttributes.t('WELCOME')} ${requestAttributes.t('HELP')}`;
+        return responseBuilder
+            .speak(speechOutput)
+            .reprompt(speechOutput)
+            .getResponse();
+    },
+};
+
+const AboutHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest' && request.intent.name === 'AboutIntent';
+    },
+    handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        const requestAttributes = attributesManager.getRequestAttributes();
+
+        return responseBuilder
+            .speak(requestAttributes.t('ABOUT'))
+            .getResponse();
+    },
+};
+
+
+const StopHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest'
+            && (request.intent.name === 'AMAZON.NoIntent'
+            || request.intent.name === 'AMAZON.CancelIntent'
+            || request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        const requestAttributes = attributesManager.getRequestAttributes();
+        return responseBuilder
+            .speak(requestAttributes.t('STOP'))
+            .getResponse();
+    },
+};
+
+const SessionEndedHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+        return handlerInput.responseBuilder.getResponse();
+    },
+};
+
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const request = handlerInput.requestEnvelope.request;
+
+        console.log(`Error handled: ${error.message}`);
+        console.log(` Original request was ${JSON.stringify(request, null, 2)}\n`);
+
+        return handlerInput.responseBuilder
+            .speak('Sorry, I can\'t understand the command. Please say again.')
+            .reprompt('Sorry, I can\'t understand the command. Please say again.')
+            .getResponse();
+    },
+};
+
+const FallbackHandler = {
+
+
+  canHandle(handlerInput) {
+
+    const request = handlerInput.requestEnvelope.request;
+
+    return request.type === 'IntentRequest'
+
+      && request.intent.name === 'AMAZON.FallbackIntent';
+
+  },
+
+  handle(handlerInput) {
+
+    return handlerInput.responseBuilder
+
+      .speak(FALLBACK_MESSAGE)
+
+      .reprompt(FALLBACK_REPROMPT)
+
+      .getResponse();
+
+  },
+
+};
+
+
+
+const languageStrings = {
+    en: {
+        translation: {
+            WELCOME: 'Welcome to Kochi Guide!',
+            HELP: 'Say about, to hear more about the city, or say coffee, breakfast, lunch, or dinner, to hear local restaurant suggestions, or say recommend an attraction, or say, go outside. ',
+            ABOUT: 'Kochi, is a major port city on the south-west coast of India bordering the Laccadive Sea. It is part of the district of Ernakulam in the state of Kerala and is often referred to as Ernakulam.',
+            STOP: 'Okay, see you next time!',
+        },
+    },
+};
+const data = {
+    city: 'Kochi',
+    state: 'Kerala',
+    postcode: '682042',
+    restaurants: [
+        {
+            name: "Aaryas",
+            address: 'Seaport Airport Road Kakkanad',
+            phone: '0484-297-2974',
+            meals: 'breakfast, lunch',
+            description: 'A vegetarian restaurant for South Indian foods.',
+        },
+        {
+            name: 'Marriot',
+            address: 'Lulu Mall Edappally',
+            phone: '0484-717-7777',
+            meals: 'breakfast',
+            description: 'Awesome food in 5 star standards. Great ambience and great food.',
+        },
+        {
+            name: 'Pie Dosa',
+            address: 'Near Al-Ameen Public School Edappally',
+            phone: '0484-280-1778',
+            meals: 'dinner',
+            description: 'Good place to eat different varieties of dosa. Easy to spot on highway side.',
+        },
+        {
+            name: 'Ifthar',
+            address: 'KK Building Edappally',
+            phone: '989-538-9777',
+            meals: 'dinner',
+            description: 'Yummy Malabar dishes and fast service. Excellent place to go out with friends.',
+        },
+        {
+            name: 'Navras',
+            address: 'Chittoor Road Pullepady',
+            phone: '703-400-7722',
+            meals: 'lunch',
+            description: 'Cozy atmosphere, great service, and the grilled fish. It tastes awesome spicy.',
+        },
+        {
+            name: "The Burger Junction",
+            address: 'Panampilly Nagar',
+            phone: '0484-404-1510',
+            meals: 'lunch, dinner',
+            description: 'Great burgers at Great Prices.',
+        },
+        {
+            name: "Barbeque Nation",
+            address: 'Imperial Trade Center Mahatma Gandhi Rd',
+            phone: '1800-108-6060',
+            meals: 'lunch, dinner',
+            description: 'Pioneers of Live Grill Concept in India. A place known for any celebrations. Best Barbecues/kebabs & Starters. Unlimited Food including Starters, Main Course, Desserts, Kulfi’s',
+        },
+        {
+            name: "Paragon",
+            address: 'Lulu Mall Edappally',
+            phone: '0484-401-1000',
+            meals: 'lunch, dinner',
+            description: 'Enduring Malabar restaurant specialising in seafood specialties such as masala fried fish.',
+        },
+        {
+            name: "Caffe Coffee Day",
+            address: 'Lulu Mall Edappally',
+            phone: '0996-483-7700',
+            meals: 'coffee',
+            description: 'Great ambience and atmosphere. A good place to sip a coffee and chat.',
+        },
+        {
+            name: "Tonico Cafe",
+            address: 'Seaport Airport Road Kakkanad',
+            phone: '0702-532-2222',
+            meals: 'coffee, dinner',
+            description: 'This casual coffee shop serves a range of coffee drinks, smoothies and international snack food.',
+        },
+    ],
+    attractions: [
+        {
+            name: 'Fort Kochi',
+            description: 'Being a hub of Dutch, Portuguese and British Administration in colonial times, Fort Kochi incredibly, is a great place to explore European Architecture.',
+            distance: '0',
+        },
+        {
+            name: 'Cherai Beach',
+            description: 'A tranquil and pristine beach, Cherai is dotted with lush green swaying coconut plantations. There are golden sandy beaches, happening beach shacks and plenty of sea shells.',
+            distance: '23',
+        },
+        {
+            name: 'Hill Palace',
+            description: 'Heritage museum and houses many archaeological relics and imperial belongings of the Maharaja of Kochi.',
+            distance: '19',
+        },
+        {
+            name: 'Mangalavanam Bird Sanctuary',
+            description: 'Biosphere reserve located at Central Cochin is a natural habitat to many endangered and regular species of local and migratory birds, as well as mangrove vegetation',
+            distance: '7',
+        },
+    ],
+};
+
+const SKILL_NAME = 'Kochi City Guide';
+const FALLBACK_MESSAGE = `The ${SKILL_NAME} skill can\'t help you with that.  It can help you learn about Kochi if you say tell me about this place. What can I help you with?`;
+const FALLBACK_REPROMPT = 'What can I help you with?';
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+exports.handler = skillBuilder
+    .addRequestHandlers(
+        LaunchHandler,
+        AboutHandler,
+        HelpHandler,
+        StopHandler,
+        FallbackHandler,
+        SessionEndedHandler
+    )
+    .addRequestInterceptors(LocalizationInterceptor)
+    .addErrorHandlers(ErrorHandler)
+    .lambda();
